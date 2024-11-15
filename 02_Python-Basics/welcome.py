a4611e526c331758873aa8cd1ce5ec2ebd2b665a
AmithBV0606 <amithrao0606@gmail.com>
2024-11-15 23:23:29 +0530

    Lesson-2 Python basics completed.

---
+++ 02_Python-Basics/welcome.py
@@ -0,0 +1,25 @@
+# Printing on the screen
+
+line01 ="********************"
+line02 ="*                  *"
+line03 ="*     WELCOME!     *"
+         
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+print('')
+
+# Comments : Notes the we would leave for overselves or other programmers.
+
+# Single line comment
+
+"""
+This
+is
+a
+multi line
+comment
+"""
